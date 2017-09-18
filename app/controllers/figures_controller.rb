4c63d31a887c7628ca005ca6a62329c8242ca796
Allen Anderson <dande313@byu.net>
2017-09-18 19:54:04 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,41 @@
+class FiguresController < ApplicationController
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @figure.titles << Title.create(params[:title])
+    @figure.landmarks << Landmark.create(params[:landmark])
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if params[:figure][:title] != ""
+      @figure.titles << Title.create(params[:figure][:title])
+    end
+    if params[:landmark][:name] != ""
+      @figure.landmarks << Landmark.create(params[:landmark][:name])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+end
